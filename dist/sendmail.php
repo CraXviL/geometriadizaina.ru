2e710e119e74df75efe77953e88e2f3a1da491ea
CraXviL <strebkov87@yandex.ru>
2018-11-28 19:06:54 +0300

    v.1.0

---
+++ dist/sendmail.php
@@ -0,0 +1,13 @@
+<?php header("Content-type: text/html; charset=utf-8");
+	if(isset($_POST["send"])) {
+		$from = htmlspecialchars("geometriadizaina.ru");
+		$to = htmlspecialchars("geometriadizaina@mail.ru");
+		$subject = htmlspecialchars("Заявка с сайта");
+		$message = htmlspecialchars($_POST["phone"]." ".$_POST["name"]." ".$_POST["email"]);
+		$subject = "=?utf-8?B?".base64_encode($subject)."?=";
+		$headers = "From: $from\r\nReply-to: $from\r\nContent-type: text/plain; charset=utf-8\r\n";
+		if (strlen($message) != 0) {
+			mail($to, $subject, $message, $headers);
+		}
+	}
+?>
