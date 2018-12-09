f33cd46c94622a4876095dee8fd405103de0b692
Andrew <strebkov87@yandex.ru>
2018-12-09 14:02:46 +0300

    v.1.2

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,50 +1,46 @@
-var hidden = true;
-var zoom = false;
-$(document).ready(function() {
-	$(".owl-carousel").owlCarousel({
-		loop:true,
-		mouseDrag:true,
-		touchDrag:true,
-		autoplay:true,
-		autoplayTimeout:2000,
-		autoplayHoverPause:true,
-		items:3,
-		margin:20
-	});
-	scroll();
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		removalDelay: 300,
-		zoom: {
-    		enabled: true,
-    		duration: 300 // продолжительность анимации. Не меняйте данный параметр также и в CSS
-		},
-		gallery: {
-		    enabled: true,
-		    navigateByImgClick: true,
-		    preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-		}
-	});
-})
+$(document).ready(() => {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    items: 3,
+    margin: 20,
+  });
 
-function scroll() {
-	$('#menu-portfolio').click(function() {
-		var height = $('#block-main').css('height');
-		$('html').animate({scrollTop:height});
-		window.location = '#block-portfolio';
-	});
-	$('#menu-price').click(function() {
-		var height1 = parseInt($('#block-main').css('height'));
-		var height2 = parseInt($('#block-portfolio').css('height'));
-		$('html').animate({scrollTop:height1+height2});
-		window.location = '#block-price';
-	});
-	$('#menu-contact').click(function() {
-		var height1 = parseInt($('#block-main').css('height'));
-		var height2 = parseInt($('#block-portfolio').css('height'));
-		var height3 = parseInt($('#block-price').css('height'));
-		$('html').animate({scrollTop:height1+height2+height3});
-		window.location = '#block-order';
-	});
-}
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    removalDelay: 300,
+    zoom: {
+      enabled: true,
+      duration: 300, // продолжительность анимации. Не меняйте данный параметр также и в CSS
+    },
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+    },
+  });
+  // делаем плавную прокрутку по якорям
+  const height1 = parseInt($('#block-main').css('height'), 10);
+  const height2 = parseInt($('#block-portfolio').css('height'), 10);
+  const height3 = parseInt($('#block-price').css('height'), 10);
+
+  $('#menu-portfolio').click(() => {
+    $('html').animate({ scrollTop: height1 });
+    window.location = '#block-portfolio';
+  });
+
+  $('#menu-price').click(() => {
+    $('html').animate({ scrollTop: height1 + height2 });
+    window.location = '#block-price';
+  });
+
+  $('#menu-contact').click(() => {
+    $('html').animate({ scrollTop: height1 + height2 + height3 });
+    window.location = '#block-order';
+  });
+});
